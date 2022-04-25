d770778722607c2370d78b70edf5ee89366d8ca1
Tavian Hall <tavian.hall@hotmail.com>
2022-04-24 22:52:13 -0500

    Add New Car

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -28,6 +28,9 @@
             <li class="nav-item">
               <RouterLink class="nav-link" to="/about">About</RouterLink>
             </li> 
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/cars">Add New Car</RouterLink>
+            </li> 
           </ul>
         </div>
       </div>
---
+++ src/views/NewCarView.vue
@@ -0,0 +1,17 @@
+<template>
+ 	<div class="Car container">
+    	<h2>Add New Car</h2>
+    	<NewCarForm />
+  	</div>
+</template>
+
+<script>
+import NewCarForm from '@/components/NewCarForm.vue'
+export default {
+
+    data() {
+        return {};
+    },
+	components: { NewCarForm }
+};
+</script>
